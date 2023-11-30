f605c970274c8b48a4bdb5681aa74265daa69006
iamthepoe <luca1901p@gmail.com>
2023-11-30 09:51:05 -0300

    feat: hello world

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Singup from './pages/SingUp'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main className='flex flex-col h-[100vh] w-full justify-center items-center'>
+        <div className='flex flex-col w-full h-28 items-center gap-1'>
+          <h1 className='text-3xl'>Escribo</h1>
+          <h2>Inovação para o aprendizado</h2>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/SingUp.jsx
@@ -0,0 +1,9 @@
+export default function Singup(){
+    return(
+        <>
+            <main>
+                <h1 className="text-black text-2xl">Test</h1>
+            </main>
+        </>
+    )
+}
